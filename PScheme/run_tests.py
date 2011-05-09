12480ac1406809df42e25868ac4526852276b96b
Andrzej <ndrwrdck@gmail.com>
2011-05-09 17:05:34 +0900

    Changed method tests are run.

--- PScheme/run_tests.py
+++ PScheme/run_tests.py
@@ -1,6 +1,8 @@
 ﻿import unittest
-
-from PScheme.tests.test_PScheme import *
+import sys, os
 
 if __name__ == "__main__":
+    sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+    from PScheme.tests.test_PScheme import *
+
     unittest.main()
